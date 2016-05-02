2e068a5b7233972112f73b5f305b4b576ac3eda9
David Swallow <dswal3@hotmail.com>
2016-05-02 13:13:46 +1000

    V1

--- src/connect_four/connectFour.java
+++ src/connect_four/connectFour.java
@@ -8,11 +8,7 @@ import java.util.Scanner;
 
 // TODO Implement proper turns
 // TODO Find a way to work out diagonal winner
-// TODO Find a way to stop X's or O's overwriting other values in the field below its supposed to drop on
-// TODO Implement Save and Loading
-// TODO Fix multiple winner messages
 // TODO If no character is entered when dropping a piece it crashes the game
-// TODO Remove test system.out.prints
 
 
 
@@ -28,6 +24,7 @@ public class connectFour {
 		System.out.println("Would you like to load a game?");
 		String startingInput = startingIn.next();
 
+		// Load Game
 		if (startingInput.equalsIgnoreCase("yes")) {
 			try {
 				loadFrom("save.txt");
@@ -44,38 +41,11 @@ public class connectFour {
 			for(int i = 0; i < 6; i++){
 		        for(int j = 0; j < 7; j++){
 		            board[i][j] = ".";
-		            System.out.print(board[i][j] + " ");
+//		            System.out.print(board[i][j] + " ");
 		        }
 		        System.out.print("\n");
 		    };
-
-
-
-//		// Creates the initial board visually, setting all values to .
-//		// This creates an elegant looking board visually
-//		// Without creating this board first, during the scanner any input submitted will not be valid
-//		for(int i = 0; i < 6; i++){
-//	        for(int j = 0; j < 7; j++){
-//	            board[i][j] = ".";
-//	            System.out.print(board[i][j] + " ");
-//	        }
-//	        System.out.print("\n");
-//	    }
-
-		System.out.print("\n");
-		// This is to display the lines/characters to select
-        System.out.print("a b c d e f g \n");
-	    System.out.print("Player 1 use lowercase a/b/c/d/e/f/g \n");
-	    System.out.print("Player 2 use UPPERCASE A/B/C/D/E/F/G \n");
-//	    System.out.print("\n");
-//
-//		try {
-//			loadFrom("save.txt");
-//			printGrid();
-//		} catch (FileNotFoundException e) {
-//			System.err.println("Could not load game: " + e.getLocalizedMessage());
-//			e.printStackTrace();
-//		}
+		printGrid();
 
 
 	    // Opens up the scanner to allow input for the user
@@ -93,20 +63,22 @@ public class connectFour {
 	        	// Uses the ascii codes of the letters ABCDEFG and converts them into an integer by taking 65
 	        	// This is used to place an character into the arrays 0123456
 	            int column = ascii -65;
-	            System.out.println("X , Column: " + column);
+//	            System.out.println("X , Column: " + column);
 	            putPlayerOnGrid(column, "X");
 	        }
 
 	        // If the character used is a small character it places an O
-	        if(ascii >= 97 && ascii <= 103){
+	        else if(ascii >= 97 && ascii <= 103){
 	        	// Uses the ascii codes of the letters abcdefg and converts them into an integer by taking 97
 	        	// This is used to place an character into the arrays 0123456
 	            int column = ascii-97;
-	            System.out.println("Y , Column: "+ column);
+//	            System.out.println("O , Column: "+ column);
 	            putPlayerOnGrid(column,"O");
 	        }
 
-	        printGrid();
+	        else{
+	        	System.out.println("Invalid Command, please use another Character.\n");
+	        }
 
 			try {
 				saveTo("save.txt");
@@ -114,10 +86,7 @@ public class connectFour {
 				System.err.println("Could not save game: " + e.getLocalizedMessage());
 				e.printStackTrace();
 			}
-
 	    }
-
-
 	}
 
 	public static String[] printGrid(){
@@ -138,8 +107,8 @@ public class connectFour {
 	    System.out.print("\n");
         System.out.print("a b c d e f g \n");
 	    System.out.print("\n");
-	    System.out.print("Player 1 use lowercase a/b/c/d/e/f/g \n");
-	    System.out.print("Player 2 use UPPERCASE A/B/C/D/E/F/G \n");
+	    System.out.print("Player 1 use lowercase a/b/c/d/e/f/g to place a peice\n");
+	    System.out.print("Player 2 use UPPERCASE A/B/C/D/E/F/G to place a peice\n");
 	    System.out.print("\n");
 		return lines;
 	}
@@ -151,7 +120,7 @@ public class connectFour {
 
 	        String pos = board[i][position];
 
-	        // If the column does not equal . eg. O or X, then place it above that tile
+	        // If the column does not equal ., then place it above that tile
 	        if(!pos.equals(".")) {
 
 	            System.out.println("Placing: " + playerXorO + " above another slot");
@@ -161,11 +130,13 @@ public class connectFour {
 	            } else {
 	                System.out.println("Invalid Move!");
 	            }
+//	        return;
+	      
 	        }
 // 			This checks to see if the  column selected is equal to the bottom column, do not subtract 1 to the column
 //	        This avoids missing the row at the bottom due to arrays being 1 below a number. Eg. 0/1/2/3/4/5, instead of 1/2/3/4/5/6
 	        if(i == 5 && pos.equals(".")){
-	        	System.out.println("Placing shit");
+	        	System.out.println("Placing Piece");
 	            board[i][position] = playerXorO;
 	        }
 	    }
@@ -229,14 +200,6 @@ public class connectFour {
 	public static void loadFrom(String fileName) throws FileNotFoundException{
 		Scanner in = new Scanner(new FileReader(fileName));
 
-//		while(in.hasNext()){
-//			for (int row = 0; row <3; row++){
-//				for (int col = 0; col <3; col++){
-//					board[row][col] = in.next();
-//				}
-//			}
-//		}
-
 		while(in.hasNext()){
 			for(int i = 0; i < 6; i++){
 				for(int j = 0; j < 7; j++){
@@ -244,17 +207,6 @@ public class connectFour {
 				}
 			}
 		}
-
-
-
-//		while(in.hasNext()){
-//	    for(int i = 0; i < 6; i++){
-//	        for(int j = 0; j<7; j++){
-//	        	 out.print(board[i][j] + " ");
-//	        }
-//	        out.print("\n");
-//	    }
-//		}
 	}
 
 }
